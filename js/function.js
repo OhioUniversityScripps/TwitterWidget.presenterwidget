25e6e6824a7522199c71be2ea594094ee917ec68
Ricky Chilcott <ricky@rbsavvy.com>
2015-08-26 09:53:41 -0400

    Adding data-dnt attribute to <a></a> and cleaning up whitespace

--- js/function.js
+++ js/function.js
@@ -9,31 +9,32 @@ widgetFunctions = function() {
   this.init = function() {
     // reload window every `sedna_config.Refresh` seconds
     refreshTime = parseInt(sedna_config.Refresh) * 1000;
-    console .log("Refresh time " + refreshTime + " milliseconds");
-        
+    console.log("Refresh time " + refreshTime + " milliseconds");
+
     widgetFunctions.buildTwitterWidget("widgetContainer", sedna_config);
 
-    
     setInterval(function(){
       widgetFunctions.buildTwitterWidget("widgetContainer", sedna_config);
     }, refreshTime);
-  }  
-  
+  }
+
   this.buildTwitterWidget = function(parentDivId, options) {
     console.log("Loading or reloading...");
     twitterWidgetId = "twitterWidget";
-    
+
     parentDiv = document.getElementById(parentDivId);
     twitterWidget = document.getElementById(twitterWidgetId);
-    
+
     if (twitterWidget != null) {
       parentDiv.removeChild(twitterWidget);
     }
-    
+
+    // Create the <a class="twitter-timeline" data-dnt="true" href="widgetUURL" data-widget-id="WidgetID">Tweets from https://twitter.com/OU_MDIA/lists/recent-tweets</a>
     var widget = document.createElement("a");
     widget.setAttribute("class", "twitter-timeline");
     widget.setAttribute("id", twitterWidgetId);
     widget.setAttribute("href", options.WidgetURL);
+    widget.setAttribute("data-dnt", "true");
     widget.setAttribute("data-widget-id", options.WidgetID);
     widget.setAttribute("data-theme", options.WidgetTheme);
     widget.setAttribute("data-link-color", options.WidgetLinkColor);
@@ -41,7 +42,7 @@ widgetFunctions = function() {
     widget.setAttribute("width", options.Width);
     widget.setAttribute("height", options.Height);
     widget.setAttribute("lang", "EN");
-    
+
     widget.innerHTML = options.LinkText;
 
     parentDiv.appendChild(widget);
